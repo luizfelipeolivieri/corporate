abdf7c42ccc4d0a3bfa9d96a3ca3720eae68d019
Luiz Felipe Olivieri <83732763+luizfelipeolivieri@users.noreply.github.com>
2026-03-10 17:14:57 -0300

    Delete js directory

--- js/app.js
+++
@@ -1,8 +0,0 @@
-
-import { renderSidebar } from "../components/sidebar.js"
-import { renderNavbar } from "../components/navbar.js"
-import { router } from "./router.js"
-
-renderSidebar()
-renderNavbar()
-router()
